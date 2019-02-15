e3a9deb15c7ff4cc8a8e341f4f44ce3c692b70b9
DIego Villafuerte Soraiz <diegovillafuertesoraiz@gmail.com>
2019-02-15 15:40:21 -0600

    interrupciones de botones

--- practica2/2.2_interrupciones/2.1.5_promedioEnsamblador/2.1.5_promedioEnsamblador.ino
+++ practica2/2.2_interrupciones/2.1.5_promedioEnsamblador/2.1.5_promedioEnsamblador.ino
@@ -6,21 +6,12 @@ void loop(){
      int dst;
      asm volatile(
      
-      "ldi r16, 4      \n\t"
-      "ldi r18, 10     \n\t"
+      "ldi r16, 10     \n\t" //Esto es la N --> El número de números que va a promediar
+      "ldi r18, 10     \n\t" //
       "ldi r20, 20     \n\t"
       "ldi r22, 30     \n\t"
-//      "mul r20, r20    \n\t"
       "mul r20, r20    \n\t"
-//      "movw r20, r0    \n\t"
-      "movw %0, r0     \n\t"
-      
-//    "movw %0, r20     \n\t"
-
-//      "mul r18, r22     \n\t"
-//      "movw r18, r0    \n\t"
-//      "movw r18, r0    \n\t"
-//      "sub r18, r17     \n\t"
+      "movw %0, r0     \n\t" //Esto pasa el contendio del registro 0 a la variable dst para sacarlo por Serial
         : "=r" (dst)
     );
     Serial.println(dst);
