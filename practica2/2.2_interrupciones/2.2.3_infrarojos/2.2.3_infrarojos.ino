329c561af3222345db2c3a8f3d19892ad9683a5d
DIego Villafuerte Soraiz <diegovillafuertesoraiz@gmail.com>
2019-02-22 09:00:01 -0600

    Practica 2

--- practica2/2.2_interrupciones/2.2.3_infrarojos/2.2.3_infrarojos.ino
+++ practica2/2.2_interrupciones/2.2.3_infrarojos/2.2.3_infrarojos.ino
@@ -1,26 +1,33 @@
  // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
+int inp = 30;
 int contador = 0;
 int estado = 0;
  
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.  
-  Serial.begin(9600);  
-  cli();
-  DDRD &= ~(1 << DDD1);
-  PORTD |= (1 << PORTD1);
-  EICRA |= (1 << ISC10);
-  EICRA |= (1 << ISC11);
-  EIMSK |= (1 << INT1);
-  sei();
+  // initialize the digital pin as an output.
+  Serial.begin(9600);
+  pinMode(inp, INPUT);
+//  cli();
+//  DDRD &= ~(1 << DDD1);
+//  PORTD |= (1 << PORTD1);
+//  EICRA |= (1 << ISC10);
+//  EIMSK |= (1 << INT1); //Es port 20
+//  sei();  
+    
 }
+
+
 ISR(INT1_vect)
   {
   contador = contador+1;
   }
 // the loop routine runs over and over again forever:
 void loop() {
+  estado = digitalRead(inp);
+  if(estado){
+    contador = contador + 1;
+  }
   Serial.println(contador);
-  delay(100);
 }
